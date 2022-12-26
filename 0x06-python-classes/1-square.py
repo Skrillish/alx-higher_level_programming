f9207d8415cf36e92a1733ffd39b6210e7dfaace
Skrillish <oluwasegunsamson444@gamil.com>
2022-12-25 18:28:43 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A Square Class module"""
+
+
+class Square:
+    """A class for creating a mathematical Square"""
+
+    def __init__(self, size):
+        """Instantiate a Square object"""
+        self.__size = size
