84778911799ae029790effd357ee1ae19d988d46
Skrillish <oluwasegunsamson444@gamil.com>
2022-12-25 18:50:39 -0800

    103-magic_class.py

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Magic Class Module
+"""
+
+import math
+
+
+class MagicClass:
+    """Magic class"""
+    def __init__(self, radius=0):
+        """Initializes a Magic object"""
+        if type(radius) is not int:
+            if type(radius) is not float:
+                raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """Calculates the area of the circle"""
+        return math.pi * (self.__radius ** 2)
+
+    def circumference(self):
+        """Calculates the circumference of the circle"""
+        return 2 * math.pi * self.__radius
